4ff459cace57af77379508f3165b84e788800acc
Shivkumar Dhabade <47687386+shivkumar20@users.noreply.github.com>
2024-08-22 21:34:47 +0530

    some changes

--- src/main/java/com/Employee/EmpManagement/EmpServiceImpl.java
+++ src/main/java/com/Employee/EmpManagement/EmpServiceImpl.java
@@ -3,35 +3,52 @@ package com.Employee.EmpManagement;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmpServiceImpl implements EmpService {
 
-    List<Employee> employees = new ArrayList<>();
+   
+    EmployeeRepository employeeRepository;
 
     @Override
     public String CreateEmp(Employee employee) {
-        employees.add(employee);
+        EmployeeEntity employeeEntity = new EmployeeEntity();
+        BeanUtils.copyProperties (employee,employeeEntity);
+        employeeRepository.save(employeeEntity);
+        // employees.add(employee);
         return "added successfully";
     }
 
     @Override
     public List<Employee> ReadEmp() {
+        
+        List<EmployeeEntity>employeesList = employeeRepository.findAll();
+        List<Employee> employees = new ArrayList<>();
+
+        for (EmployeeEntity employeeEntity : employeesList) {
+            // employees.add(employeeEntity);
+            Employee e = new Employee();
+            e.setName(employeeEntity.getName());
+            e.setEmail(employeeEntity.getEmail());
+            e.setPhone(employeeEntity.getPhone());
+            employees.add(e);
+        }
         return employees;
     }
 
-    // @Override
-    // public boolean DeleteEmp(Long id) {
-    //    employees.remove(id);
-    //    return true;
-    // }
+   
 
     @Override
    public boolean DeleteEmp(Long id) {
-    for (Employee employee : employees) {
-        if (employee.getId().equals(id)) {
-            employees.remove(employee);
-            return true;
-        }
-    }
+    // for (Employee employee : employees) {
+    //     if (employee.getId().equals(id)) {
+    //         employees.remove(employee);
+    //         return true;
+    //     }
+    // }
     return false; 
   }
 
